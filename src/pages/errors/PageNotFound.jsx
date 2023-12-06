db20d4214c16840734e2402fd21419f0b5e83d7b
ratulkuri <ratulkuri@gmail.com>
2023-12-06 08:43:52 +0600

    fix: Error page design

--- src/pages/errors/PageNotFound.jsx
+++ src/pages/errors/PageNotFound.jsx
@@ -1,7 +1,7 @@
 const PageNotFound = () => {
     return (
-        <div>
-            404 | Page Not Found
+        <div className="w-100 h-screen d-flex justify-content-center align-items-center">
+            <span style={{ fontWeight: 500, fontSize: 20 }}>404 | Page Not Found</span>
         </div>
     );
 }
