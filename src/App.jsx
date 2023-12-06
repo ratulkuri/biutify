cda8586a58e9d0a99963308deabbe75fbfa2dbcf
ratulkuri <ratulkuri@gmail.com>
2023-12-06 08:26:02 +0600

    remove unnecessery lines

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,7 @@ import './App.css'
 import DefaultLayout from './components/layouts/DefaultLayout';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import Contact from './pages/Contact';
 import PageNotFound from './pages/errors/PageNotFound';
-import AuthLayout from './components/layouts/AuthLayout';
-import Login from './pages/auth/Login';
 
 function App() {
   return (
